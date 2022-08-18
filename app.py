3d47f3a4230f80280a19627f436b0f7ecb6db2dd
Rishabh <rishabhbhatia57@gmail.com>
2022-08-18 17:08:32 +0530

    Added first page as index.html

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ cors = CORS(app)
 
 @app.route('/')
 def index():
-    return "<h3>Python Flask API is running...</h3>"
+    return render_template("index.html")
 
 @app.route('/GetJWT')
 def GetJWT():
     getToken = getJWT()
     return getToken
 
-@app.route('/Firstpage')
-def Firstpage():
-    return render_template("index.html")
+# @app.route('/Firstpage')
+# def Firstpage():
+#     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,4 +1,11 @@
-  var url = 'https://vetinatableau.azurewebsites.net/GetJWT';
+let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+    let newSettings = 'Desktop'
+
+    if (isMobile) { //this returns true or false
+        newSettings = 'Phone'
+    }
+
+var url = 'https://vetinatableau.azurewebsites.net/GetJWT';
     fetch(url, {
         method: 'GET',
     }).then(res => res.json())
@@ -6,6 +13,7 @@
             console.log(response['token']);
             var elementVar = document.getElementById("tableauViz");
             elementVar.setAttribute("token", response['token']);
+            elementVar.setAttribute("device", newSettings);
             document.getElementById("loadingcheck").innerHTML = "<div class='alert alert-success' role='alert'><h3 id='loadingMessage'>Loaded successfully!</h3></div>";
             
         })
