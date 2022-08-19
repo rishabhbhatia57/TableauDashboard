1f5863f7636923123fde3565db89db74787ce164
Rishabh <rishabhbhatia57@gmail.com>
2022-08-19 12:42:07 +0530

    changed app to express app

---
+++ src/server.js
@@ -0,0 +1,27 @@
+
+let path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use('/js', express.static('./node_modules/bootstrap/dist/js/')); // Redirect bootstrap JS
+app.use('/js', express.static('./node_modules/jquery/dist/')); // Redirect JS jQuery
+app.use('/js', express.static('./node_modules/powerbi-client/dist/')) // Redirect JS PowerBI
+app.use('/css', express.static('./node_modules/bootstrap/dist/css/')); // Redirect CSS bootstrap
+app.use('/public', express.static('./public/')); // Use custom JS and CSS files
+
+
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../views/index.html'));
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
